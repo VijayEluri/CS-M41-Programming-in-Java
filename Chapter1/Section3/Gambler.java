ecbffcbe988eadb32df1f997d3889fb546018560
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-09-24 09:44:54 +0100

    Improved Simulation of Markov chain with two absorbing boundaries.
    A special case of a random walk, namely a "drunkard's walk" (or "gambler's
    ruin").

---
+++ Chapter1/Section3/Gambler.java
@@ -0,0 +1,33 @@
+// Oliver Kullmann, 24.9.2009 (Swansea)
+
+class Gambler {
+  public static void main(String[] args) {
+
+  try {
+    final int stake = Integer.parseInt(args[0]);
+    final int goal = Integer.parseInt(args[1]);
+    final int N = Integer.parseInt(args[2]);
+    int wins = 0, losses = 0;
+
+    for (int t = 0; t < N; ++t) {
+      int cash = stake;
+      while (cash > 0 && cash < goal)
+        if (Math.random() < 0.5) ++cash;
+        else --cash;
+      if (cash == goal) ++wins;
+      if (cash == 0) ++losses;
+    }
+    assert wins+losses == N; // ATTENTION: Prerequisites are 0 <= stake <= goal and goal > 0, N >= 0. Left as exercise to check this.
+
+    System.out.println(wins + " wins in " + N + " repetitions, and " + losses + " losses.");
+  }
+
+  catch(ArrayIndexOutOfBoundsException e) {
+    System.err.println("ERROR[RandomInt]: Three parameter values are needed, namely the integers stake, goal and T (number of repetitions).");
+  }
+  catch(RuntimeException e) {
+    System.err.println("ERROR[RandomInt]: The parameters must be integers within the range from -2147483648 to 2147483647.");
+  }
+
+  }
+}
