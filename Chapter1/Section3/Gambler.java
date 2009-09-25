226407ef0b9a7b1ad0034267b3eb4df9e8b3f4ed
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-09-25 09:15:11 +0100

    Replace "N" by "T" (as in the book).

--- Chapter1/Section3/Gambler.java
+++ Chapter1/Section3/Gambler.java
@@ -6,10 +6,10 @@ class Gambler {
   try {
     final int stake = Integer.parseInt(args[0]);
     final int goal = Integer.parseInt(args[1]);
-    final int N = Integer.parseInt(args[2]);
+    final int T = Integer.parseInt(args[2]);
     int wins = 0, losses = 0;
 
-    for (int t = 0; t < N; ++t) {
+    for (int t = 0; t < T; ++t) {
       int cash = stake;
       while (cash > 0 && cash < goal)
         if (Math.random() < 0.5) ++cash;
@@ -17,13 +17,13 @@ class Gambler {
       if (cash == goal) ++wins;
       if (cash == 0) ++losses;
     }
-    assert wins+losses == N; // ATTENTION: Prerequisites are 0 <= stake <= goal and goal > 0, N >= 0. Left as exercise to check this.
+    assert wins+losses == T; // ATTENTION: Prerequisites are 0 <= stake <= goal and goal > 0, T >= 0. Left as exercise to check this.
 
-    System.out.println(wins + " wins in " + N + " repetitions, and " + losses + " losses.");
+    System.out.println(wins + " wins in " + T + " trials, and " + losses + " losses.");
   }
 
   catch(ArrayIndexOutOfBoundsException e) {
-    System.err.println("ERROR[RandomInt]: Three parameter values are needed, namely the integers stake, goal and T (number of repetitions).");
+    System.err.println("ERROR[RandomInt]: Three parameter values are needed, namely the integers stake, goal and T (number of trials).");
   }
   catch(RuntimeException e) {
     System.err.println("ERROR[RandomInt]: The parameters must be integers within the range from -2147483648 to 2147483647.");
