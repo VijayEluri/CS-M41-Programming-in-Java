7630aa51b27d3c56d68d1a6baa3a44ca761bf15d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-10-08 19:26:48 +0100

    Slightly improved program for computing averages.

---
+++ Chapter1/Section5/Average.java
@@ -0,0 +1,29 @@
+// Oliver Kullmann, 8/10/2009 (Swansea)
+
+class Average { 
+  public static void main(String[] args) { 
+    int N = 0;           // number input values
+    double sum = 0.0;    // sum of input values
+
+    // read data and compute statistics
+    try {
+      while (!StdIn.isEmpty()) {
+        final double x = StdIn.readDouble();
+        ++N;
+        sum += x;
+      }
+    }
+    catch (RuntimeException e) {
+      StdOut.println("ERROR[Average]: Input errror.");
+      return;
+    }
+
+    StdOut.println();
+    if (N == 0)
+      StdOut.println("No input.");
+    else {
+      final double average = sum / N;
+      StdOut.println("Average is " + average);
+    }
+  }
+}
