dbdd2065e0a26760fef3f5c19086ab81524ace16
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-11-29 11:05:33 +0000

    Completed Hand-class.
    Has not been compiled yet.

--- Courseworks/200910/Two/Hand.java
+++ Courseworks/200910/Two/Hand.java
@@ -2,10 +2,19 @@ class Hand {
 
   // Provides poker hands in a standardised form, first sorted by descending
   // ranks, and then "sorted" by suites as by the order given implicitly in
-  // class Suit.
+  // class Suit. Thus two objects of type Hand are equal if and only if
+  // the hands are equal as sets.
+
+  // Three constructors are given, for reading five cards, an array of cards,
+  // or for reading from standard input.
+
+  // If the hand does not consist of five different cards, then an exception
+  // is thrown.
+
+  // Access to cards by get(i), where 1 <= i <= 5.
 
   public static final int hand_size = 5;
-  public static final int num_hands = 2598960;
+  public static final int num_hands = 2598960; // = binom(52,5)
 
   public Hand(final Card c1, final Card c2, final Card c3, final Card c4, final Card c5) {
     cards = new Card[hand_size];
@@ -40,20 +49,70 @@ class Hand {
     return cards[i-1];
   }
 
+  public String toString() {
+    String result = "";
+    for (int i = 0; i < hand_size; ++i) {
+      final Card c = cards[i];
+      result += c.rank.toString();
+      result += " of ";
+      result += c.suit.toString();
+      result += "; ";
+    }
+    return result;
+  }
+
+  public boolean equals(final Hand h) {
+    for (int i = 0; i < hand_size; ++i)
+      if (! h.cards[i].equals(cards[i]))
+        return false;
+    return true;
+  }
+
   private final Card[] cards;
 
-  private static void sort_by_ranks(Card[] h) {
-    // YYY
+  // Sorting by selection sort:
+  public static void stable_sort_by_ranks(final Card[] h) {
+    for (int i = 0; i < h.length-1; ++i) {
+      int index_min = i;
+      for (int j = i+1; j < h.length; ++j)
+        if (h[j].rank < h[index_min].rank)
+          index_min = j;
+      if (index_min != i) {
+        final Card temp = h[i];
+        h[i] = h[index_min];
+        h[index_min] = temp;
+      }
+    }
   }
-  private static void sort_by_suites(Card[] h) {
-    // YYY
+  public static void stable_sort_by_suites(final Card[] h) {
+    for (int i = 0; i < h.length-1; ++i) {
+      int index_min = i;
+      for (int j = i+1; j < h.length; ++j)
+        if (h[j].suit < h[index_min].suit)
+          index_min = j;
+      if (index_min != i) {
+        final Card temp = h[i];
+        h[i] = h[index_min];
+        h[index_min] = temp;
+      }
+    }
   }
-  private void prepare_hand() {
-    sort_by_ranks(cards);
-    sort_by_suites(cards);
+  /* Remarks: Actually we only need sort_by_suites to be stable.
+     If we wanted to use a sorting algorithm from the Java library, then
+     we needed some means to "tell" that algorithm the sorting criterions;
+     by what we learned in the module, yet we cannot provide such means.
+  */
+
+  public static void check_all_different(final Card[] h) {
     for (int i = 0; i < hand_size - 1; ++i)
-      if (cards[i].equals(cards[i+1]))
+      if (h[i].equals(h[i+1]))
         throw RuntimeException("Two identical cards were found in a hand.");
+  }
+
+  private void prepare_hand() {
+    stable_sort_by_ranks(cards);
+    stable_sort_by_suites(cards);
+    check_all_different(cards);
   }
 
 }
