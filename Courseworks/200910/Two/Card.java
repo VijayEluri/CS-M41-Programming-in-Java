70619fd24ef3d6b18bc6f1431210289d3a46462d
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-11-29 13:36:14 +0000

    Reordered rank and suit (now rank first).
    Also added missing public-qualifiers.

--- Courseworks/200910/Two/Card.java
+++ Courseworks/200910/Two/Card.java
@@ -1,32 +1,32 @@
 class Card {
 
-  // A "card" as a pair of suit and (card-)rank.
+  // A "card" as a pair of (card-)rank and suit.
 
   public static final int num_cards = Suit.num_suites * CardRank.num_ranks;
 
-  public final Suit suit;
   public final CardRank rank;
+  public final Suit suit;
 
-  public Card(final Suit s, final CardRank r) {
-    suit = s;
+  public Card(final CardRank r, final Suit s) {
     rank = r;
+    suit = s;
   }
   public Card(final int i) {
     assert i >= 0;
     assert i < num_cards;
-    suit = new Suit(i / CardRank.num_ranks);
     rank = new CardRank(i % CardRank.num_ranks);
+    suit = new Suit(i / CardRank.num_ranks);
   }
 
   public int index() {
-    suit.index * CardRank.num_ranks + rank.index;
+    return suit.index * CardRank.num_ranks + rank.index;
   }
 
-  String toString() {
+  public String toString() {
     return rank.toString() + " of " + suit.toString();
   }
 
-  boolean equals(Card c) {
-    return c.suit.equals(suit) && c.rank.equals(rank);
+  public boolean equals(Card c) {
+    return c.rank.equals(rank) && c.suit.equals(suit);
   }
 }
