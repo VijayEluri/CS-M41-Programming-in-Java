84f8a1fb01b5c3ed3ef9db6932c02fbc11e4102c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-03 00:17:57 +0000

    Provided solution for determining the other cards.
    And tests.

--- Courseworks/200910/Two/Evaluation.java
+++ Courseworks/200910/Two/Evaluation.java
@@ -7,8 +7,15 @@ class Evaluation {
     assert h != null;
     hand = h;
     other_cards = new Card[Card.num_cards - Hand.hand_size];
-    // Set the 47 cards in other_cards to the cards not in h:
-    // XXX
+    int[] deck = new int[Card.num_cards];
+    for (int i = 0; i < Card.num_cards; ++i)
+      deck[i] = i;
+    for (int i = 1; i <= Hand.hand_size; ++i)
+      deck[h.get(i).index()] = -1;
+    int next_index = 0;
+    for (int i = 0; i < Card.num_cards; ++i)
+      if (deck[i] != -1)
+        other_cards[next_index++] = new Card(deck[i]);
   }
 
   // For an exchange-request e, compute the array of possible outcomes
@@ -29,4 +36,14 @@ class Evaluation {
 
   private final Hand hand;
   private final Card[] other_cards;
+
+
+  public static void main(String[] args) {
+    final Bank b = new Bank(1);
+    final Hand h = b.orig_hand(1);
+    System.out.println(h);
+    final Evaluation E = new Evaluation(h);
+    for (int i = 0; i < 47; ++i)
+      System.out.println(E.other_cards[i]);
+  }
 }
