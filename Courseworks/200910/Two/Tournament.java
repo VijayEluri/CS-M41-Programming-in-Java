438c8489d25096635c58dc56e48cf67f9ea2fdc1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-04 00:52:29 +0000

    Completed solution for the Tournament-program.

--- Courseworks/200910/Two/Tournament.java
+++ Courseworks/200910/Two/Tournament.java
@@ -7,10 +7,19 @@ class Tournament {
   // pair 1 <= i < j <= Strategy.number_strategies of strategies from Strategy,
   // play N rounds, showing the difference of the number of games won by
   // strategy i minus the number of games won by strategy j.
-  // N is possibly very large, and thus type int is not appropriate.
   public static void main(String[] args) {
-    // XXX
-    Storage S = new Storage(Strategy.number_strategies); // for the statistics
-    // XXX
+    if (args.length == 0) {
+      System.err.println("ERROR[Tournament]: One parameter is neded, the number N of games.");
+      return;
+    }
+    final long N = Long.parseLong(args[0]);
+    final Storage S = new Storage(Strategy.number_strategies);
+    for (long t = 1; t <= N; ++t) {
+      final Storage R = TwoPlayers.play();
+      for (int i = 1; i < Strategy.number_strategies; ++i)
+        for (int j = i+1; j <= Strategy.number_strategies; ++j)
+          S.set(i,j, S.get(i,j)+R.get(i,j));
+    }
+    System.out.println(S);
   }
 }
