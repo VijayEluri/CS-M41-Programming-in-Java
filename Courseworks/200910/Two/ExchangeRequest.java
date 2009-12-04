b56ffad8a25feaa3fb58e6ba1d2d5fd3e0e2ecc3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2009-12-04 01:48:52 +0000

    Improved and extended ExchangeRequest.

--- Courseworks/200910/Two/ExchangeRequest.java
+++ Courseworks/200910/Two/ExchangeRequest.java
@@ -18,30 +18,87 @@ class ExchangeRequest {
     assert e.get_index(2) == 5;
   */
 
+  public static final int max_exchanges = 2;
+  public static final int total_num_requests = 1 + 5 + 10;
+
   public final int number_cards;
 
   public ExchangeRequest() {
     number_cards = 0;
     exchange_indices = null;
   }
-  ExchangeRequest(int[] exchange_indices_) {
+  public ExchangeRequest(final int e1) {
+    assert e1 >= 1;
+    assert e1 <= Hand.hand_size;
+    number_cards = 1;
+    exchange_indices = new int[1];
+    exchange_indices[0] = e1;
+  }
+  public ExchangeRequest(final int e1, final int e2) {
+    assert e1 >= 1;
+    assert e1 <= Hand.hand_size;
+    assert e2 >= 1;
+    assert e2 <= Hand.hand_size;
+    assert e1 < e2;
+    number_cards = 2;
+    exchange_indices = new int[2];
+    exchange_indices[0] = e1;
+    exchange_indices[1] = e2;
+  }
+  public ExchangeRequest(int[] exchange_indices_) {
     assert exchange_indices_ != null;
-    assert(exchange_indices_.length <= 2);
+    assert(exchange_indices_.length <= max_exchanges);
     for (int i = 0; i < exchange_indices_.length; ++i) {
       assert(exchange_indices_[i] >= 1);
-      assert(exchange_indices_[i] <= 5);
+      assert(exchange_indices_[i] <= Hand.hand_size);
     }
-    if (exchange_indices_.length == 2)
+    if (exchange_indices_.length == max_exchanges)
       assert exchange_indices_[0] < exchange_indices_[1];
     exchange_indices = exchange_indices_;
     number_cards = exchange_indices.length;
   }
 
-  int get_index(int i) {
+  public int get_index(final int i) {
     assert i >= 1;
     assert i <= number_cards;
     return exchange_indices[i-1];
   }
 
+  public String toString() {
+    if (number_cards == 0)
+      return "0:.";
+    if (number_cards == 1)
+      return "1: " + exchange_indices[0] + ".";
+    return "2: " + exchange_indices[0] + ", " + exchange_indices[1] + ".";
+  }
+
+  public boolean equals(final ExchangeRequest E) {
+    if (E.number_cards != number_cards) return false;
+    if (number_cards == 0) return true;
+    if (number_cards == 1) return E.exchange_indices[0] == exchange_indices[0];
+    return E.exchange_indices[0] == exchange_indices[0] && E.exchange_indices[1] == exchange_indices[1];
+  }
+
+  public static ExchangeRequest[] all_requests() {
+    final ExchangeRequest[] E = new ExchangeRequest[total_num_requests];
+    E[0] = new ExchangeRequest();
+    for (int i = 1; i <= Hand.hand_size; ++i)
+      E[i] = new ExchangeRequest(i);
+    int next_index = Hand.hand_size+1;
+    for (int i = 1; i <= Hand.hand_size-1; ++i)
+      for (int j =i+1; j <= Hand.hand_size; ++j)
+        E[next_index++] = new ExchangeRequest(i,j);
+    return E;
+  }
+
   private final int[] exchange_indices;
+
+
+  public static void main(String[] args) {
+    final ExchangeRequest[] E = all_requests();
+    assert E.length == total_num_requests;
+    for (int i = 0; i < total_num_requests; ++i)
+      System.out.println(E[i]);
+  }
+
 }
