177cfb07f10a125641990e6e895c109113fb2948
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-10-15 07:25:04 +0100

    Program to demonstrate left-associativity of concatenation.

---
+++ Chapter1/Section2/LeftAssociative.java
@@ -0,0 +1,11 @@
+// Oliver Kullmann, 15/10/2010 (Swansea)
+
+public class LeftAssociative {
+  public static void main(String[] args) {
+    final int a = Integer.parseInt(args[0]);
+    final int b = Integer.parseInt(args[1]);
+    System.out.println(a + " + " + b + " = " + a + b); // not what is meant
+    System.out.println(a + " + " + b + " = " + (a + b));
+    System.out.println(a + b + " + " + "13" + " = " + (a + b + 13)); // understand this
+  }
+}
