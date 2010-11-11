b18c8bbea28c5c96334534e1742a84b2eb56070e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-11-11 07:07:18 +0000

    Added final-specifications.

--- Chapter1/Section5/PlotFilter.java
+++ Chapter1/Section5/PlotFilter.java
@@ -12,13 +12,13 @@
 
 public class PlotFilter { 
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         // read in bounding box and rescale
-        double x0 = StdIn.readDouble();
-        double y0 = StdIn.readDouble();
-        double x1 = StdIn.readDouble();
-        double y1 = StdIn.readDouble();
+        final double x0 = StdIn.readDouble();
+        final double y0 = StdIn.readDouble();
+        final double x1 = StdIn.readDouble();
+        final double y1 = StdIn.readDouble();
         StdDraw.setXscale(x0, x1);
         StdDraw.setYscale(y0, y1);
 
@@ -27,8 +27,8 @@ public class PlotFilter {
 
         // plot points, one at a time
         while (!StdIn.isEmpty()) {
-            double x = StdIn.readDouble();
-            double y = StdIn.readDouble();
+            final double x = StdIn.readDouble();
+            final double y = StdIn.readDouble();
             StdDraw.point(x, y);
         }
 
