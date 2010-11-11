0bc63989cbd41e08559fd2a8c2eec471faf10303
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-11-11 06:20:24 +0000

    Some improvements.

--- Chapter1/Section5/Average.java
+++ Chapter1/Section5/Average.java
@@ -1,8 +1,8 @@
 // Oliver Kullmann, 8/10/2009 (Swansea)
 
 class Average { 
-  public static void main(String[] args) { 
-    int N = 0;           // number input values
+  public static void main(final String[] args) { 
+    long N = 0;           // number input values
     double sum = 0.0;    // sum of input values
 
     // read data and compute statistics
