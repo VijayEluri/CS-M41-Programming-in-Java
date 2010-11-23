ef9d3267d22dada0ccc44c8b3ee7c87e94e5a3f3
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-11-23 11:40:54 +0000

    Improved implementation according to the course-standards.

--- Chapter2/Section2/Completed/RandomPoints.java
+++ Chapter2/Section2/Completed/RandomPoints.java
@@ -1,10 +1,11 @@
 public class RandomPoints {
-   public static void main(String args[]) {
-      int N = Integer.parseInt(args[0]);
-      for (int i = 0; i < N; i++) {
-         double x = StdRandom.gaussian(0.5, 0.2);
-         double y = StdRandom.gaussian(0.5, 0.2);
-         StdDraw.point(x, y);
-      }
-   }
+  public static void main(final String args[]) {
+    final int N = Integer.parseInt(args[0]);
+    for (int i = 0; i < N; ++i) {
+      double x = StdRandom.gaussian(0.5, 0.2);
+      double y = StdRandom.gaussian(0.5, 0.2);
+      StdDraw.point(x, y);
+    }
+    System.out.println("Finished.");
+  }
 }
