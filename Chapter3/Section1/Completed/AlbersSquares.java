aba0f21bf1603c6f1130bfbeda72ba030e7fe7c2
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-11-30 11:21:33 +0000

    Finished template AlbersSquares.

--- Chapter3/Section1/Completed/AlbersSquares.java
+++ Chapter3/Section1/Completed/AlbersSquares.java
@@ -24,13 +24,13 @@ public class AlbersSquares {
         final int r1 = Integer.parseInt(args[0]);
         final int g1 = Integer.parseInt(args[1]);
         final int b1 = Integer.parseInt(args[2]);
-        final Color c1 = XXX;
+        final Color c1 = new Color(r1, g1, b1);
 
         // second color
         final int r2 = Integer.parseInt(args[3]);
         final int g2 = Integer.parseInt(args[4]);
         final int b2 = Integer.parseInt(args[5]);
-        final Color c2 = XXX;
+        final Color c2 = new Color(r2, g2, b2);
 
         // first Albers square
         StdDraw.setPenColor(c1);
@@ -39,9 +39,9 @@ public class AlbersSquares {
         StdDraw.filledSquare(.25, .5, .1);
 
         // second Albers square
-        StdDraw.setPenColor(XXX);
+        StdDraw.setPenColor(c2);
         StdDraw.filledSquare(.75, .5, .2);
-        StdDraw.setPenColor(XXX);
+        StdDraw.setPenColor(c1);
         StdDraw.filledSquare(.75, .5, .1);
     } 
 } 
