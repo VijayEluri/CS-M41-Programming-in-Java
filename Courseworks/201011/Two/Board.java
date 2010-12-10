e09ea043f8da345329eba01086cc7b2871a05162
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-12-10 08:18:41 +0000

    First tests for normal moves.

--- Courseworks/201011/Two/Board.java
+++ Courseworks/201011/Two/Board.java
@@ -285,7 +285,15 @@ class Board {
       }
       // testing normal moves
       {
-
+        final Board b = new Board();
+        /*
+        b.do_normal_white_move('e','2','e','4');
+        assert(b.toFEN().equals("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"));
+        b.do_normal_black_move('c','7','c','5');
+        assert(b.toFEN().equals("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"));
+        b.do_normal_white_move('g','1','f','3');
+        assert(b.toFEN().equals("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"));
+        */
       }
       // testing seting and reseting
       {
