0e2d4e9e4591ee14940ea34f7b01ddb1586e6411
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2010-12-17 17:14:56 +0000

    Removed comments from move-sequence.

--- Courseworks/201011/Two/Game.java
+++ Courseworks/201011/Two/Game.java
@@ -56,9 +56,22 @@ class Game {
 
   // checks for syntactical correctness (only!); returns -1 in case of
   // a syntactical error, and the number of halfmoves (>= 0) otherwise:
-  public static int valid_move_sequence(final String seq) {
+  public static int valid_move_sequence(String seq) {
+    seq = remove_comments(seq);
+    if (seq.isEmpty()) return -1;
     // code will be provided YYY
     return 0;
+  }
+  // removes comments, returns empty string in case of error:
+  private static String remove_comments(String seq) {
+    for (int opening_bracket = seq.indexOf("{"); opening_bracket != -1;
+         opening_bracket = seq.indexOf("{")) {
+      final int closing_bracket = seq.indexOf("}");
+      if (closing_bracket < opening_bracket) return "";
+      seq = seq.substring(0,opening_bracket) + seq.substring(closing_bracket+1);
+    }
+    if (seq.contains("}")) return "";
+    else return seq;
   }
 
   private char[][] fill_move_seq() {
@@ -101,6 +114,14 @@ class Game {
         "1. e4 e5 2. Nf3 Nc6 3. Bb5 {This opening is called Ruy Lopez.} 3... a6 4. Ba4 Nf6 5. 0-0 Be7 6. Re1 b5 7. Bb3 d6 8. c3 0-0 9. h3 Nb8 10. d4 Nbd7 11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5 Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21. Nc4 Nxc4 22. Bxc4 Nb6 23. Ne5 Rae8 24. Bxf7+ Rxf7 25. Nxf7 Rxe1+ 26. Qxe1 Kxf7 27. Qe3 Qg5 28. Qxg5 hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. f3 Bc8 34. Kfe Bf5 35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6 Nf2 42. g4 Bd3 43. Re6 1/2-1/2",
         "",
         true);
+    }
+    // syntax check
+    {
+      assert(remove_comments("").equals(""));
+      assert(remove_comments("{").equals(""));
+      assert(remove_comments("}").equals(""));
+      assert(remove_comments("{}").equals(""));
+      assert(remove_comments("xyz { jyt } kjh { bvc po5 } ").equals("xyz  kjh  "));
     }
   }
 }
