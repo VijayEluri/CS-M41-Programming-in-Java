e22acb1816d57de3a8c10940bae333ec32e6d74f
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-11-17 19:52:50 +0000

    Editorial improvements.

--- Chapter2/Section1/Max.java
+++ Chapter2/Section1/Max.java
@@ -6,7 +6,7 @@ public class Max {
 
   public static void main(final String[] args) {
     final int N = args.length;
-    int[] a = new int[N];
+    final int[] a = new int[N];
     for (int i = 0; i < N; ++i)
       a[i] = Integer.parseInt(args[i]);
     System.out.println(max(a));
