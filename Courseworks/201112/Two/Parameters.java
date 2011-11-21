671fdc7e4403f5e3e203aa6f252b7b6d814fc6b4
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-11-21 23:39:44 +0000

    Implemented parsing of the parameter line.

--- Courseworks/201112/Two/Parameters.java
+++ Courseworks/201112/Two/Parameters.java
@@ -5,6 +5,9 @@ class Parameters {
   public static final String error_header = "ERROR[" + GeneralisedTicTacToe.program_name + "]: ";
   public static final String message_no_param = "The parameters are k, m, n " +
     "and one of \"hh\", \"hc\", \"ch\", \"cc\".";
+  public static final String message_num_param = "Four parameters are needed, not ";
+  public static final String message_parsing = "The first three parameters must be integers.";
+  public static final String message_mode = "The fourth parameter must be one of \"hh\", \"hc\", \"ch\", \"cc\".";
 
   /*
     In case of errors, error-messages are output to System.err and
@@ -15,16 +18,35 @@ class Parameters {
   */
   public static int[] process_parameters(final String[] args) {
     assert(args != null);
-    int[] result = null;
     if (args.length == 0) {
       error_message(message_no_param);
-      return result;
+      return null;
     }
-
-    // XXX to be completed XXX
-    if (args[3] == "hh") result[3] = GeneralisedTicTacToe.mode_hh;
-    // XXX to be completed XXX
-
+    if (args.length != 4) {
+      error_message(message_num_param + args.length + ".");
+      return null;
+    }
+    int K,M,N;
+    try {
+      K = Integer.parseInt(args[0]);
+      M = Integer.parseInt(args[1]);
+      N = Integer.parseInt(args[2]);
+    }
+    catch (final Exception e) {
+      error_message(message_parsing);
+      return null;
+    }
+    int m = 0;
+    if (args[3].equals("hh")) m = GeneralisedTicTacToe.mode_hh;
+    else if (args[3].equals("hc")) m = GeneralisedTicTacToe.mode_hc;
+    else if (args[3].equals("ch")) m = GeneralisedTicTacToe.mode_ch;
+    else if (args[3].equals("cc")) m = GeneralisedTicTacToe.mode_cc;
+    if (m == 0) {
+      error_message(message_mode);
+      return null;
+    }
+    int[] result = new int[4];
+    result[0] = K; result[1] = M; result[2] = N; result[3] = m;
     return result;
   }
 
