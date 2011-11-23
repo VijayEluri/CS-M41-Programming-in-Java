12126da938c0e7012ba6d7bfc18572c203b8dcb0
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-11-23 15:31:08 +0000

    Update according to branch cw2.

--- Courseworks/201112/Two/Parameters.java
+++ Courseworks/201112/Two/Parameters.java
@@ -1,10 +1,21 @@
 // Oliver Kullmann, 21.11.2011 (Swansea)
 
+/*
+  Provides function process_parameters(args):
+  - parses the parameters from the command line and returns them in an array;
+  - in case of input errors, messages are printed on standard error, and
+    null is returned.
+*/
+
 class Parameters {
 
   public static final String error_header = "ERROR[" + GeneralisedTicTacToe.program_name + "]: ";
   public static final String message_no_param = "The parameters are k, m, n " +
     "and one of \"hh\", \"hc\", \"ch\", \"cc\".";
+  public static final String message_num_param = "Four parameters are needed, not ";
+  public static final String message_parsing = "The first three parameters must be integers.";
+  public static final String message_positive = "The first three parameters must be > 0.";
+  public static final String message_mode = "The fourth parameter must be one of \"hh\", \"hc\", \"ch\", \"cc\".";
 
   /*
     In case of errors, error-messages are output to System.err and
@@ -15,16 +26,20 @@ class Parameters {
   */
   public static int[] process_parameters(final String[] args) {
     assert(args != null);
-    int[] result = null;
     if (args.length == 0) {
       error_message(message_no_param);
-      return result;
+      return null;
     }
-
-    // XXX to be completed XXX
-    if (args[3] == "hh") result[3] = GeneralisedTicTacToe.mode_hh;
-    // XXX to be completed XXX
-
+    if (args.length != 4) {
+      error_message(message_num_param + args.length + ".");
+      return null;
+    }
+    int K,M,N;
+    // XXX to be completed: reading K,M,N XXX
+    int m = 0;
+    // XXX to be completed: reading m XXX
+    int[] result = new int[4];
+    result[0] = K; result[1] = M; result[2] = N; result[3] = m;
     return result;
   }
 
