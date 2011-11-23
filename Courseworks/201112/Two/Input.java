f60bb54d1b4db832838fcb69d0dd0ecde970579e
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-11-23 12:13:52 +0000

    Added specifications.

--- Courseworks/201112/Two/Input.java
+++ Courseworks/201112/Two/Input.java
@@ -1,10 +1,18 @@
 // Oliver Kullmann, 21.11.2011 (Swansea)
 
+/*
+  Provides function read_move(M,N):
+  - returns (in an array), after successful reading from standard input, the
+    two valid coordinates for a move;
+  - invalid inputs are ignored (in pairs);
+  - if the end of standard input is encountered, null is returned.
+*/
+
 class Input {
 
   // returns an array of length 2, with two valid coordinates,
-  // or null the end of standard input was encountered; does not throw any
-  // exception:
+  // or null if the end of standard input was encountered; does not throw any
+  // exceptions:
   public static int[] read_move(final int M, final int N) {
     boolean waiting = true;
     int i=0,j=0;
