c80af5eb3cd0a2ec66170e9db8267a5bd94c9e74
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-11-28 21:27:28 +0000

    Improved error handling.

--- Courseworks/201112/One/ModularPower.java
+++ Courseworks/201112/One/ModularPower.java
@@ -4,10 +4,12 @@ class ModularPower {
   static final String errmess = "ERROR[ModularPower]: ";
   static final int errcode_args = 1;
   static final int errcode_n = 2;
+  static final int errcode_int = 3;
+
   public static void main(final String[] args) {
     if (args.length != 3) {
       System.err.println(errmess + "Precisely three arguments are needed, while you entered " +
-        args.length + " many.");
+        args.length + " arguments.");
       System.exit(errcode_args);
     }
     final int n = Integer.parseInt(args[2]);
@@ -19,10 +21,16 @@ class ModularPower {
       System.out.println(0);
       return;
     }
-    final int a = Integer.parseInt(args[0]) % n;
-    final int e = Integer.parseInt(args[1]);
-    long power = 1;
-    for (int i = 0; i < e; ++i) power = (power * a) % n;
-    System.out.println(power);
+    try {
+      final int a = Integer.parseInt(args[0]) % n;
+      final int e = Integer.parseInt(args[1]);
+      long power = 1;
+      for (int i = 0; i < e; ++i) power = (power * a) % n;
+      System.out.println(power);
+    }
+    catch (final NumberFormatException e) {
+      System.err.println(errmess + "One of the parameters was not an integer.");
+      System.exit(errcode_int);
+    }
   }
 }
