f7e7da1f7354ae33ddc368783df1173ac39d1554
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2011-12-11 11:54:42 +0000

    Using System.out instead of StdOut (we do not use additional functionality).

--- Chapter3/Section2/PassByValue.java
+++ Chapter3/Section2/PassByValue.java
@@ -3,14 +3,14 @@ public class PassByValue {
     a    = 7;
     b[3] = 7;
     c    = "seven";
-    StdOut.println(a + " " + b[3] + " " + c);
+    System.out.println(a + " " + b[3] + " " + c);
   }
   public static void main(String[] args) {
     int a = 3;
     int[] b = { 0, 1, 2, 3, 4, 5 };
     String c = "three";
-    StdOut.println(a + " " + b[3] + " " + c);
+    System.out.println(a + " " + b[3] + " " + c);
     update(a, b, c);
-    StdOut.println(a + " " + b[3] + " " + c);
+    System.out.println(a + " " + b[3] + " " + c);
   }
 }
