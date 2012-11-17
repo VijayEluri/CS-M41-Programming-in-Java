3e8db128c0c8fb2a6a6e3ef59dda52d66b55f9e1
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-11-17 09:27:34 +0000

    Template for Count-exercise.

---
+++ Chapter2/Section2/Count.java
@@ -0,0 +1,29 @@
+class Count {
+
+  private static XXX count(XXX a, XXX) {
+    assert(a != null);
+    XXX result = XXX;
+    if (a.length == 0) XXX;
+    for (int i = 0; i < N; ++i) XXX;
+    XXX;
+  }
+  
+  private static XXX output(XXX c) {
+    assert(c != null);
+    for (int i = 0; i < c.length; ++i) System.out.print(XXX);
+    XXX;
+  }
+
+  public static void main(final String[] args) {
+    final int l = args.length;
+    if (l == 0) {
+      System.err.println("ERROR[Count]: at least the number of trials is needed.");
+      System.exit(1);
+    }
+    final int N = Integer.parseInt(args[0]);
+    final double[] a = new double[l-1];
+    for (int i = 1; i < l; ++i) a[i-1] = Double.parseDouble(args[i]);
+    output(count(a, N));
+    System.out.println();
+  }
+}
