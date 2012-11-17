976d8b2ca40e6cf286df6c0da7532cfc02ab9002
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-11-17 09:18:34 +0000

    Counting example for week 8.

---
+++ Chapter2/Section2/Completed/Count.java
@@ -0,0 +1,29 @@
+class Count {
+
+  private static int[] count(final double[] a, final int N) {
+    assert(a != null);
+    int[] result = new int[a.length];
+    if (a.length == 0) return result;
+    for (int i = 0; i < N; ++i) ++result[StdRandom.discrete(a)];
+    return result;
+  }
+  
+  private static void output(final int[] c) {
+    assert(c != null);
+    for (int i = 0; i < c.length; ++i) System.out.print(c[i] + " ");
+    return;
+  }
+
+  public static void main(final String[] args) {
+    final int l = args.length;
+    if (l == 0) {
+      System.err.println("ERROR[Count]: at least the number of trials is needed.");
+      System.exit(1);
+    }
+    final int N = Integer.parseInt(args[0]);
+    final double[] a = new double[l-1];
+    for (int i = 1; i < l; ++i) a[i-1] = Double.parseDouble(args[i]);
+    output(count(a, N));
+    System.out.println();
+  }
+}
