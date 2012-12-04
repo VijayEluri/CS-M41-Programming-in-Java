082cd5b904b0fddc88f5bed4dc410133465291ca
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-12-04 18:01:08 +0000

    Simplified solution for first coursework, first question.

---
+++ Courseworks/201213/One/Statistics2.java
@@ -0,0 +1,20 @@
+public class Statistics {
+  public static void main(final String[] args) {
+    final int N = args.length;
+    System.out.println(N);
+    if (N == 0) return;
+    double min = Double.POSITIVE_INFINITY;
+    double max = Double.NEGATIVE_INFINITY;
+    double sum = 0.0;
+    for (int i = 0; i < N; ++i) {
+      final double v = Double.parseDouble(args[i]);
+      if (v < min) min = v;
+      if (v > max) max = v;
+      sum += v;
+    }
+    System.out.println(min);
+    System.out.println(max);
+    System.out.println(sum);
+    System.out.println(sum/N);
+  }
+}
