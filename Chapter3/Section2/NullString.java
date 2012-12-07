fd58f3fd124dbf5086fca4a25e770c6fb446f06c
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2012-12-07 08:43:00 +0000

    Demo program for "null".

---
+++ Chapter3/Section2/NullString.java
@@ -0,0 +1,27 @@
+class NullString {
+  public static void main(final String[] args) {
+    String s1;
+    /* s1 has the value null now, however we can't check this directly,
+       since then the compiler will say "not initialised" (try it out).
+    */
+    String s2 = null;
+    System.out.println(s2);
+    System.out.println(s2 == null);
+    // not possible: s2.length() (null-pointer exception --- try it!)
+    s1 = "a";
+    /* s1 is now not null, but you can't print its memory address, since it's
+       a string, whose *value* will be printed instead.
+    */
+    System.out.println(s1 != null);
+    s1 = null;
+    System.out.println(s1 == null);
+    System.out.println(s1 == s2);
+    s2 = ""; // that's a perfect string, not different from any other strng
+    System.out.println(s2 != null);
+    System.out.println(s1 != s2);
+
+    String[] a = new String[2];
+    for (int i = 0; i < a.length; ++i)
+      System.out.println(i + ": " + (a[i] == null));
+  }
+}
