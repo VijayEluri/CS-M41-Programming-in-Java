735f9e243fdf47b14da0f5b2802e41a84ff462e5
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2014-11-22 05:21:27 +0000

    Editorial improvements.

--- Courseworks/201415/One/Cases.java
+++ Courseworks/201415/One/Cases.java
@@ -3,20 +3,17 @@ class Cases {
     final int nargs = args.length;
     if (nargs == 0) {
       System.out.println("Nothing to be done.");
-      System.exit(0);
+      return;
     }
     if (nargs == 1) {
       System.out.println("Hello, " + args[0] + "!");
-      System.exit(0);
+      return;
     }
     if (nargs == 2) {
-      final int a = Integer.parseInt(args[0]);
-      final int b = Integer.parseInt(args[1]);
-      if (b == 0)
-        System.out.println("a = " + a);
-      else
-        System.out.println(a + " % " + b + " = " + (a % b));
-      System.exit(0);
+      final int a = Integer.parseInt(args[0]), b = Integer.parseInt(args[1]);
+      if (b == 0) System.out.println("a = " + a);
+      else System.out.println(a + " % " + b + " = " + (a % b));
+      return;
     }
     if (nargs == 3) {
       final double a = Double.parseDouble(args[0]);
@@ -29,14 +26,13 @@ class Cases {
       else if (a <= c) System.out.println(b + " " + a + " " + c);
       else if (c < b) System.out.println(c + " " + b + " " + a);
       else System.out.println(b + " " + c + " " + a);
-      System.exit(0);
+      return;
     }
     if (nargs >= 4 && nargs <= 6) {
       final boolean even = nargs % 2 == 0;
       final int maj = (even) ? nargs/2 : nargs/2+1;
       final int[] a = new int[nargs];
-      for (int i = 0; i < nargs; ++i)
-        a[i] = Integer.parseInt(args[i]);
+      for (int i = 0; i < nargs; ++i) a[i] = Integer.parseInt(args[i]);
       int i = 0; boolean stop = false;
       while (i <= nargs / 2 && ! stop) {
         final int b = a[i];
@@ -48,7 +44,7 @@ class Cases {
         }
         ++i;
       }
-      System.exit(0);
+      return;
     }
     {
       final int rem = Integer.parseInt(args[0]);
@@ -56,7 +52,7 @@ class Cases {
         long sum = 0;
         for (int i = 1; i < nargs; ++i) sum += Integer.parseInt(args[i]);
         System.out.println(sum);
-        System.exit(0);
+        return;
       }
       final int[] count = new int[rem];
       for (int i = 1; i < nargs; ++i) {
