dcbd4f8b40dfdd9a400d52ab1f874461fb545206
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2016-01-10 17:37:07 +0000

    More on NaN.

--- Courseworks/201516/Two/Misuse.java
+++ Courseworks/201516/Two/Misuse.java
@@ -1,5 +1,9 @@
 class Misuse {
   public static void main(final String[] args) {
+    System.out.println(Double.NaN > 0);
+    System.out.println(Double.NaN < 0);
+    System.out.println(Double.NaN == 0);
     System.out.println(Math.max(Double.NaN, 5));
+    System.out.println(Math.max(Double.NaN, Double.POSITIVE_INFINITY));
   }
 }
