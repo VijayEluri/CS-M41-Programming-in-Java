da4915250eab99226f7a274881d2786616a65675
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2016-01-11 13:13:57 +0000

    More on special Double-values.

--- Courseworks/201516/Two/Misuse.java
+++ Courseworks/201516/Two/Misuse.java
@@ -5,5 +5,8 @@ class Misuse {
     System.out.println(Double.NaN == 0);
     System.out.println(Math.max(Double.NaN, 5));
     System.out.println(Math.max(Double.NaN, Double.POSITIVE_INFINITY));
+    System.out.println(Double.MAX_VALUE < Double.POSITIVE_INFINITY);
+    System.out.println(Double.MIN_VALUE > 0);
+    System.out.println(-Double.MAX_VALUE > Double.NEGATIVE_INFINITY);
   }
 }
