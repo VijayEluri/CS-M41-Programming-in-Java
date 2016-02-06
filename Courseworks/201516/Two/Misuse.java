385462f51bf8345cb6736e4dd05991ee4f9bb749
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2016-02-06 19:57:36 +0000

    Addition on NaN.

--- Courseworks/201516/Two/Misuse.java
+++ Courseworks/201516/Two/Misuse.java
@@ -3,6 +3,8 @@ class Misuse {
     System.out.println(Double.NaN > 0);
     System.out.println(Double.NaN < 0);
     System.out.println(Double.NaN == 0);
+    System.out.println(Double.NaN == Double.NaN);
+    System.out.println(Double.NaN != Double.NaN);
     System.out.println(Math.max(Double.NaN, 5));
     System.out.println(Math.max(Double.NaN, Double.POSITIVE_INFINITY));
     System.out.println(Double.MAX_VALUE < Double.POSITIVE_INFINITY);
