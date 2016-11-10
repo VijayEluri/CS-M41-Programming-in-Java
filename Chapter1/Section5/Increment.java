bad76b0a2bb787a7002dc86f479fcd3ed78c7aca
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2016-11-10 04:28:14 +0000

    New small demo.

---
+++ Chapter1/Section5/Increment.java
@@ -0,0 +1,15 @@
+// Oliver Kullmann, 10.11.2016 (Swansea)
+
+class Increment {
+  public static void main(final String[] args) {
+    int i = 0;
+    System.out.println(++i);
+    System.out.println(i);
+    System.out.println(i++);
+    System.out.println(i);
+    System.out.println(--i);
+    System.out.println(i);
+    System.out.println(i--);
+    System.out.println(i);
+  }
+}
