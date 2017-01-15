b79145d928903e26eee1b1e4b3ab214fb72f3775
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2017-01-15 12:50:44 +0000

    Corrected implementation of sum_income: a[i]==null now handled.

--- Courseworks/201617/Two/Admin.java
+++ Courseworks/201617/Two/Admin.java
@@ -21,7 +21,8 @@ class Admin {
   public static long sum_income(final Person[] a) {
     if (a == null) return 0;
     long sum = 0;
-    for (int i = 0; i < a.length; ++i) sum += a[i].income();
+    for (int i = 0; i < a.length; ++i)
+      if (a[i] != null) sum += a[i].income();
     return sum;
   }
   
