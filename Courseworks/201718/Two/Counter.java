f94b3f8d371d6bb2d47861d38fd70d698fb9fb23
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2017-11-28 22:55:05 +0000

    Class to be used for second coursework.

---
+++ Courseworks/201718/Two/Counter.java
@@ -0,0 +1,50 @@
+class Counter {
+  public static final int bound = 1000000;
+  public final String name;
+  private int count;
+  public Counter(final String n) {
+    assert(bound >= 0);
+    assert(bound < Integer.MAX_VALUE);
+    assert(! n.equals(""));
+    name = n;
+    count = 0;
+  }
+  public int count() {
+    assert(count >= 0);
+    assert(count <= bound);
+    return count;
+  }
+  public boolean inc() {
+    assert(count >= 0);
+    assert(count <= bound);
+    if (count < bound) {
+      ++count;
+      return true;
+    }
+    else return false;
+  }
+  public String toString() {
+    assert(! name.equals(""));
+    assert(count >= 0);
+    assert(count <= bound);
+    return name + ": " + count;
+  }
+  public boolean equals(final Counter other) {
+    assert(! name.equals(""));
+    assert(count >= 0);
+    assert(count <= bound);
+    return other != null && name.equals(other.name);
+  }
+  
+  public static void main(final String[] args) {
+    final Counter c1 = new Counter("a");
+    System.out.println(c1);
+    final Counter c2 = new Counter("b");
+    System.out.println(c2);
+    System.out.println(c1.equals(c2));
+    c1.inc();
+    System.out.println(c1.count());
+    c2.inc(); c2.inc();
+    System.out.println(c1 + " " + c2);
+  }
+}
