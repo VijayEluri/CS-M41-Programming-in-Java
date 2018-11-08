2e24fd429d53d3f70a61f41571ccd787174ef464
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-11-08 05:19:42 +0000

    Some improvements.

--- Chapter1/Week05/Deck352.java
+++ Chapter1/Week05/Deck352.java
@@ -1,7 +1,10 @@
 class Deck352 {
+// How the distinction between 32 and 52 is done is a little exercise in
+// code reading.
   public static void main(final String args[]) {
     final String suit[] = {"Clubs", "Diamonds", "Hearts", "Spades" };
     final int nr_suits = suit.length;
+    assert(nr_suits == 4);
     final boolean short_deck = args.length != 0;
     final int nr_ranks = (short_deck) ? 8 : 13;
     final String rank[] = (short_deck) ?
@@ -9,6 +12,7 @@ class Deck352 {
       : new String[]{"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
     assert(rank.length == nr_ranks);
     final int nr_cards = nr_suits * nr_ranks;
+    assert(nr_cards == ((short_deck) ? 32 : 52));
 
     final String deck[] = new String[nr_cards];
     for (int r = 0; r < nr_ranks; ++r)
