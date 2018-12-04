81a0ea6fdbca6d9ef0220a46b0cb9685568e61a9
Oliver Kullmann <O.Kullmann@Swansea.ac.uk>
2018-12-04 13:08:53 +0000

    Small improvement.

--- Chapter3/Week10/Length.java
+++ Chapter3/Week10/Length.java
@@ -6,7 +6,7 @@ class Length {
     assert(a.length == 3);
         
     /* Array-objects and String-objects have *fixed sizes*, so both classes could hold
-       *final* instance-variables with the length. (So changing size can only happen via
+       *final* instance-variables with the length. (Thus changing size can only happen via
        a new object; elements can be changed, but not the whole thing.)
        The Array class exposes that data-member directly, which is fine since it's *final*.
        The String class puts a wrapper around that data-member --- this wouldn't be needed,
